a80eeb8c63f6c92a3f500adee04f608b8a56cb13
Ethan McCartney <mccare6@rpi.edu>
2024-04-13 15:10:58 -0400

    made genetic algo faster

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -1,4 +1,4 @@
-import random, torch, tqdm, math
+import random, torch, tqdm
 import numpy as np
 
 import networkx as nx
@@ -6,79 +6,59 @@ from util import read_nxgraph
 from util import obj_maxcut_batch
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print('Device set to', device)
 
-def random_genome(length: int):
-	return (torch.rand(length) > 0.5).int()
-
-class Individual():
-	def __init__(self, genome:np.ndarray, temp:int = 0.10):
-		self.genome = genome
-		self.fitness = 0
-
-	def offspring(self, parent, temp=0.10):
-		child_chromosome = torch.zeros_like(self.genome, dtype=torch.int)
-		for i in range(len(self.genome)):
-			prob = random.random()
-			if prob < (1 - temp) / 2:
-				child_chromosome[i] = self.genome[i]
-			elif prob < (1 - temp):
-				child_chromosome[i] = parent.genome[i]
-			else:
-				child_chromosome[i] = int(random.random() < 0.5)
-		return Individual(genome=child_chromosome)
-
-def genetic_simulated_annealing(num_generations: int, population_size:int, init_temp: int, graph: nx.Graph, seed: int, do_pbar=True):
+def genetic_vectorized(num_generations: int, population_size:int, init_temp: int, graph: nx.Graph, seed: int, do_pbar=True):
 	np.random.seed(seed)
 	random.seed(seed)
 	torch.manual_seed(seed)
 
 	temp = init_temp
 	best_score = 0
+	survivor_percent = 0.10
 	best_solution = None
 
-	adj_matrix = torch.tensor(nx.to_numpy_array(graph)).to(device)
+	adj_matrix = torch.tensor(nx.to_numpy_array(graph), device=device)
 	num_nodes = graph.number_of_nodes()
 
 	if do_pbar:
 		pbar = tqdm.tqdm(range(num_generations), f'Simulated Annealing, Score: {best_score}')
 
 	#generate initial population
-	population = [Individual(random_genome(num_nodes)) for _ in range(population_size)]
-	
+	population = (torch.rand((population_size, num_nodes), device=device)<0.5).int()
 	for k in range(num_generations):
-		genomes = torch.stack([individual.genome for individual in population], dim=0)
-		#print(genomes)
 
+		# Update temperature/mutation rate
 		temp = init_temp * (1 - (k) / num_generations)
-		#temp = init_temp / math.log(2 + k)
-		#temp = init_temp / math.log(k + 2)
-		#temp = init_temp / math.log(k + 2, num_generations + 2) * math.log(k + 2)
-		#temp = 1 - ((k+1)/num_generations)
 
-
-		fitnesses = obj_maxcut_batch(genomes, adj_matrix)
+		# Get fitness scores/Maxcut scores
+		fitnesses = obj_maxcut_batch(population, adj_matrix)
 		gen_best = torch.max(fitnesses).item()
 		if gen_best > best_score:
 			best_score = gen_best
-
-		for i in range(len(population)):
-			population[i].fitness = fitnesses[i]
-
-		population.sort(key=lambda x: x.fitness, reverse=True)
+		
+		# Sort population by fittness scores/Maxcut scores
+		sorted_indices = torch.argsort(fitnesses, descending=True)
+		population = population[sorted_indices]
 
 		#save top 10%
-		next_gen = []
-		survivor_percent = 0.1
+		next_gen = torch.zeros_like(population)
 		num_survive = int(population_size*survivor_percent)
-		next_gen.extend(population[:num_survive])
+		next_gen[:num_survive] = population[:num_survive]
 
-		#generate offspring from the top 50%
-		for _ in range(population_size - num_survive):
-			parent1 = random.choice(population[:population_size//2])
-			parent2 = random.choice(population[:population_size//2])
-			child = parent1.offspring(parent2, temp=temp)
-			next_gen.append(child)
-  
+		# Generate offspring from the top 50%
+		parent_indices = torch.randint(0, population_size // 2, ((population_size - num_survive)*2,)) 	#generate vector of length (population_size - num_survive)*2, of random parents from the top 50% (*2 bc u need 2 parents)
+		parent1 = population[parent_indices[:parent_indices.shape[0]//2]] 								#parent 1 is from first half of random top 50%
+		parent2 = population[parent_indices[parent_indices.shape[0]//2:]] 								#parent 2 is from second half of random top 50%
+		crossover_points = torch.randint(0, 2, (population_size - num_survive, num_nodes), device=device)
+		offspring = parent1 * crossover_points + parent2 * (1 - crossover_points) 						#1 for parent1 gene, and 0 for parent 2 gene
+
+		# Apply mutations
+		mutations = (torch.rand(offspring.shape[0], num_nodes, device=device) < temp)
+		offspring ^= mutations
+
+		# Set next generation
+		next_gen[num_survive:] = offspring
 		population = next_gen
 
 		if do_pbar:
@@ -88,21 +68,45 @@ def genetic_simulated_annealing(num_generations: int, population_size:int, init_
 
 
 if __name__ == '__main__':
-	# genome_length = 10
-	# test = Individual(random_genome(genome_length))
-	# test2 = Individual(random_genome(genome_length))
-	# print('parent #1:', test.genome)
-	# print('parent #2:', test2.genome)
-	# offspring = test.offspring(test2)
-	# print('offspring:', offspring.genome)
 
+	'''
+	NOTE: population size doesn't change ideal starting temp,
+	for 1000 generations it seems like 0.05 is the ideal starting temp, but number of itterations might change that
+ 
+	NOTE: after futher testing it seems like an initial temperature of 0.05 is ideal for any number of itterations. 
+  	it might not be for lower population sizes, but with larger population sizes it seems to converge to 0.05
+	'''
+
+	# params = {
+	# 	'num_generations': 2000,
+	# 	'population_size': 1000, 
+	# 	'init_temp': 0.05, 
+	# 	'graph': read_nxgraph('data/syn/powerlaw_500_ID0.txt'), 
+	# 	'seed': 0, 
+	# 	'do_pbar': True
+	# } #= 1460 = 0.088 gamma
+ 
 	params = {
-		'num_generations': 3000,
-		'population_size': 10, 
-		'init_temp': 0.10, 
+		'num_generations': 10000,
+		'population_size': 1000, 
+		'init_temp': 0.05, 
 		'graph': read_nxgraph('data/syn/powerlaw_500_ID0.txt'), 
 		'seed': 0, 
 		'do_pbar': True
 	}
 
-	genetic_simulated_annealing(**params)
+	print(f'temp: {params["init_temp"]}, seed: {params["seed"]}, population size: {params["population_size"]}')
+	genetic_vectorized(**params)
+
+	# Test all 500 node graphs
+	# for i in range(30):
+	# 	params = {
+	# 		'num_generations': 2000,
+	# 		'population_size': 1000, 
+	# 		'init_temp': 0.05, 
+	# 		'graph': read_nxgraph(f'data/syn/powerlaw_500_ID{i}.txt'), 
+	# 		'seed': 0, 
+	# 		'do_pbar': True
+	# 	}
+	# 	print(f'500 node graph ID{i}')
+	# 	genetic_vectorized(**params)
